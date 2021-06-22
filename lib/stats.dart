c840a8058779c5127b5e391626e1fff295f54e36
Bharath Bathula <mailtobharathbathula@gmail.com>
2021-06-22 14:52:19 +0530

    ui changes

--- lib/stats.dart
+++ lib/stats.dart
@@ -8,101 +8,104 @@ class Stats extends StatelessWidget {
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context)!.settings.arguments as ScreenArguments;
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('Stats'),
-      // ),
       body: SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage(
-                    'assets/hero.png',
+          child: args.win > args.lost
+              ? Container(
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(
+                            'assets/hero.png',
+                          ),
+                          fit: BoxFit.cover)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      SizedBox(
+                        height: 30,
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                              child: Text(
+                            'Wins',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 22),
+                          )),
+                          Container(
+                              child: Text(
+                            'Lost',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 22),
+                          )),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                              child: Text(
+                            args.win.toString(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 22),
+                          )),
+                          Container(
+                              child: Text(
+                            args.lost.toString(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 22),
+                          )),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      // Container(
+                      //     child:
+                      //args.win > args.lost
+                      //         ? Text(
+                      //             'You Win',
+                      //             style: TextStyle(
+                      //                 fontWeight: FontWeight.bold,
+                      //                 fontSize: 22,
+                      //                 color: Colors.green),
+                      //           )
+                      //         : args.win == args.lost
+                      //             ? Text(
+                      //                 'Its a Draw',
+                      //                 style: TextStyle(
+                      //                     fontWeight: FontWeight.bold,
+                      //                     fontSize: 22,
+                      //                     color: Colors.brown),
+                      //               )
+                      //             : Text(
+                      //                 'You Lost',
+                      //                 style: TextStyle(
+                      //                     fontWeight: FontWeight.bold,
+                      //                     fontSize: 22,
+                      //                     color: Colors.red),
+                      //               )),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.1,
+                      ),
+                      IconButton(
+                          onPressed: () {
+                            Navigator.popAndPushNamed(context, StartPage.route);
+                          },
+                          icon: Icon(
+                            Icons.home,
+                            size: 40,
+                          ))
+                    ],
                   ),
-                  fit: BoxFit.cover)),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              SizedBox(
-                height: 30,
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                      child: Text(
-                    'Wins',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
-                  )),
-                  Container(
-                      child: Text(
-                    'Lost',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
-                  )),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                      child: Text(
-                    args.win.toString(),
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
-                  )),
-                  Container(
-                      child: Text(
-                    args.lost.toString(),
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
-                  )),
-                ],
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              // Container(
-              //     child: args.win > args.lost
-              //         ? Text(
-              //             'You Win',
-              //             style: TextStyle(
-              //                 fontWeight: FontWeight.bold,
-              //                 fontSize: 22,
-              //                 color: Colors.green),
-              //           )
-              //         : args.win == args.lost
-              //             ? Text(
-              //                 'Its a Draw',
-              //                 style: TextStyle(
-              //                     fontWeight: FontWeight.bold,
-              //                     fontSize: 22,
-              //                     color: Colors.brown),
-              //               )
-              //             : Text(
-              //                 'You Lost',
-              //                 style: TextStyle(
-              //                     fontWeight: FontWeight.bold,
-              //                     fontSize: 22,
-              //                     color: Colors.red),
-              //               )),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.1,
-              ),
-              IconButton(
-                  onPressed: () {
-                    Navigator.popAndPushNamed(context, StartPage.route);
-                  },
-                  icon: Icon(
-                    Icons.home,
-                    size: 40,
-                  ))
-            ],
-          ),
-        ),
-      ),
+                )
+              : Container()),
     );
   }
 }
